f50f7fcdee167010fb08b9b4ac0811253a47b161
Rafael Barash <rafabarash@gmail.com>
2019-08-22 20:37:05 -0700

    added material ui to project

---
+++ src/theme.tsx
@@ -0,0 +1,29 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: 'black',
+    },
+  },
+  typography: {
+    fontFamily: ['Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+    htmlFontSize: 10,
+    fontSize: 14,
+  },
+  overrides: {},
+});
+
+export default theme;
